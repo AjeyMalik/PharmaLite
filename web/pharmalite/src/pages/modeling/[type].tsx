53e97676cf5c6ceadbd074b327573a37e12c77e3
Rathan <rthotireddy@contenterra.com>
2023-06-06 13:13:05 +0530

    added static path for testing for later purpose

--- web/pharmalite/src/pages/modeling/[type].tsx
+++ web/pharmalite/src/pages/modeling/[type].tsx
@@ -1,6 +1,7 @@
 import AuthLayout from "index/components/common/AuthLayout";
 import ModelingTypesComponent from "index/components/modeling/ModelingTypesComponent";
 import { getCompany } from "index/services/util/UtilService";
+// import { GetStaticPaths, GetStaticProps } from "next";
 // import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -17,14 +18,33 @@ import { useState, useEffect } from "react";
 //   };
 // };
 
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   console.log("--staticprops--", params);
+//   return {
+//     props: {
+//       params,
+//     },
+//   };
+// };
+
+// export const getStaticPaths: GetStaticPaths<{ type: string }> = async ({}) => {
+//   return {
+//     paths: [],
+//     fallback: false,
+//   };
+// };
+
 interface ModelingTypePageProps {}
 
-const ModelingTypePage: React.FunctionComponent<ModelingTypePageProps> = () => {
+const ModelingTypePage: React.FunctionComponent<ModelingTypePageProps> = (
+  // props: any
+) => {
   const [data, setData] = React.useState<any>({});
   const [companyName, setcompanyName] = useState<string>("");
   const router = useRouter();
 
   useEffect(() => {
+    // console.log("--staticpropsInUseEffect--", props);
     let queryParams = router.query;
     setData(queryParams);
     let company = getCompany();
