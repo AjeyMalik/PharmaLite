62091845097408892681f042b289bb065a89c179
AjeyMalik <94215226+AjeyMalik@users.noreply.github.com>
2023-07-11 15:42:57 +0530

    first commit

---
+++ web/pharmalite/src/pages/BMR/FacilityClearance/index.tsx
@@ -0,0 +1,33 @@
+import AuthLayout from "index/components/common/AuthLayout";
+import BMRcancel from "index/components/bmr/BMRcancel";
+import { getCompany } from "index/services/util/UtilService";
+import Head from "next/head";
+import * as React from "react";
+import { useState, useEffect } from "react";
+import BMRfacilityClearance from "index/components/bmr/BMRfacilityClearance";
+
+interface GrnPageProps {}
+
+const GrnPage: React.FunctionComponent<GrnPageProps> = () => {
+  const [companyName, setcompanyName] = useState("");
+  useEffect(() => {
+    let company = getCompany();
+    setcompanyName(company || "");
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>Home Page - {companyName}</title>
+        <meta name="description" content={"Description of " + companyName} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <React.Fragment>
+        <AuthLayout>
+          <BMRfacilityClearance/>
+        </AuthLayout>
+      </React.Fragment>
+    </>
+  );
+};
+
+export default GrnPage;
